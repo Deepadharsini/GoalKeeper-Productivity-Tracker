310a2adfdfaf1d177d4fb4fecdfa8d62e47d285b
Deepadharsini <dharsinikannan.2112@gmail.com>
2025-04-29 16:50:07 +0530

    plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.VITE_API_BASE_URL||"/GoalKeeper-Productivity-Tracker",
+  plugins: [react()],
+  base: process.env.VITE_API_BASE_URL,
 });
